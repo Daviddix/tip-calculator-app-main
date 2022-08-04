04d9de9702958c9be20879f7d43f3ae7bdea9cc7
Emmanuel Nsikan-David <en56434@gmail.com>
2022-08-04 08:40:35 +0100

    some styles

---
+++ app.js
@@ -0,0 +1,12 @@
+// variables
+let billInput = document.querySelector('.bill-input')
+
+const percentageButtons = document.querySelectorAll(".buttons-and-input-container button")
+
+const customPercentage = document.querySelector('.buttons-and-input-container input')
+
+const numberOfPeopleInput = document.querySelector('.number-input')
+
+let totalTipAmount = document.querySelector('.actual-tip-amount p')
+
+let totalPerPerson = document.querySelector('actual-total-amount p')
